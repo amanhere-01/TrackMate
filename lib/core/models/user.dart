f72a6f83022f421b4345c44040bfad68cfcfe8ae
amanhere-01 <amanhere0101@gmail.com>
2024-08-07 01:44:22 +0530

    user model

---
+++ lib/core/models/user.dart
@@ -0,0 +1,10 @@
+class User{
+  final String uid;
+  final String name;
+  final String email;
+
+  User({required this.uid, required this.name, required this.email});
+
+  // factory User.fromDatabase(User)
+
+}
