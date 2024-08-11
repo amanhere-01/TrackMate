123494cfd52e084a577b9b43b0afb36e61183373
amanhere-01 <amanhere0101@gmail.com>
2024-08-11 23:47:07 +0530

    retrieving location

--- lib/features/location/data/location_remote_data_source.dart
+++ lib/features/location/data/location_remote_data_source.dart
@@ -1,20 +1,34 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:track_mate/features/location/models/shared_location_model.dart';
+import 'package:track_mate/features/location/models/location_model.dart';
 
 abstract interface class LocationRemoteDataSource{
-  Future<void> shareLocation(SharedLocationModel sharedLocation);
+  Future<void> shareLocation(LocationModel sharedLocation);
+  Future<LocationModel> trackLocation(String code);
 }
 
 class LocationRemoteDataSourceImpl implements LocationRemoteDataSource{
   final db = FirebaseFirestore.instance;
   
   @override
-  Future<void> shareLocation(SharedLocationModel sharedLocation) async{
+  Future<void> shareLocation(LocationModel sharedLocation) async{
     try{
       final docRef = db.collection("shared_location").doc(sharedLocation.sharingCode);
-      docRef.set(sharedLocation.toMap());
-      final ggid = docRef.id;
-      print(ggid);
+      await docRef.set(sharedLocation.toMap());
+    } catch(e){
+      throw Exception(e.toString());
+    }
+  }
+
+  @override
+  Future<LocationModel> trackLocation(String code) async {
+    try{
+      final docRef = db.collection("shared_location").doc(code);
+      final docSnapshot = await docRef.get();
+      if(docSnapshot.exists){
+        return LocationModel.fromMap(docSnapshot.data() as Map<String,dynamic>);
+      } else{
+        throw Exception('Location not found for code: $code');
+      }
     } catch(e){
       throw Exception(e.toString());
     }
