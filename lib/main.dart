6c04522109702237ca2ea207339c51e24655485a
amanhere-01 <amanhere0101@gmail.com>
2024-08-12 23:54:14 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,47 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:track_mate/core/theme/theme.dart';
+import 'package:track_mate/features/auth/bloc/auth_bloc.dart';
+import 'package:track_mate/features/auth/data/auth_remote_data_source.dart';
+import 'package:track_mate/features/location/bloc/location_bloc.dart';
+import 'package:track_mate/features/location/data/location_remote_data_source.dart';
+import 'package:track_mate/features/splash_screen.dart';
+import 'package:track_mate/firebase_options.dart';
+import 'package:track_mate/features/location/presentation/pages/home_page.dart';
+import 'features/auth/presentaion/pages/sign_in_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'features/auth/pages/sign_in_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(
+    MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (context) => AuthBloc(AuthRemoteDataSourceImpl())),
+          BlocProvider(create: (context) => LocationBloc(LocationRemoteDataSourceImpl()))
+        ],
+        child: const MyApp()
+    )
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightThemeMode,
-      home: const SignInPage(),
+      home: const SplashScreen()  ,
     );
   }
 }
