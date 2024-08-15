b207e536d8ca5fd0e7b907b2ce02395e5c5c269b
amanhere-01 <amanhere0101@gmail.com>
2024-08-15 23:23:28 +0530

    fixed render issue

--- lib/features/auth/presentaion/pages/sign_in_page.dart
+++ lib/features/auth/presentaion/pages/sign_in_page.dart
@@ -59,80 +59,82 @@ class _SignInPageState extends State<SignInPage> {
                 child: Padding(
                   padding: const EdgeInsets.all(15.0),
                   child: Center(
-                      child:  GlassBox(
-                        height: 520.0,
-                        child: Column(
-                          children: [
-                            const Text(
-                                'Sign In',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 40,
-                                fontWeight: FontWeight.bold
-                              ),
-                            ),
-                            const SizedBox(height: 40,),
-                            AuthTextField(hint: 'Email', controller:emailController,isObscure: false, icon: Icons.email_outlined,),
-                            const SizedBox(height: 20,),
-                            AuthTextField(hint: 'Password', controller:passwordController,isObscure: true, icon: Icons.lock_outline,),
-                            const SizedBox(height: 40,),
-                            ElevatedButton(
-                              onPressed: (){
-                                String emptyField = validate();
-                                if(emptyField=='null'){
-                                  context.read<AuthBloc>().add(
-                                      AuthSignIn(
-                                          email: emailController.text.trim(),
-                                          password: passwordController.text.trim()
-                                      )
-                                  );
-                                } else {
-                                  showSnackbar(context, '$emptyField is empty!');
-                                }
-                              },
-                              style: ElevatedButton.styleFrom(
-                                fixedSize: Size(MediaQuery.of(context).size.width,60),
-                                backgroundColor: Colors.black,
-                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
-                              ),
-                              child: const Text(
-                                'Sign In',
+                      child:  SingleChildScrollView(
+                        child: GlassBox(
+                          height: 520.0,
+                          child: Column(
+                            children: [
+                              const Text(
+                                  'Sign In',
                                 style: TextStyle(
-                                    fontSize: 20,
-                                    color: ColorPalette.primary,
-                                    fontWeight: FontWeight.w500
+                                  color: Colors.black,
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              const SizedBox(height: 40,),
+                              AuthTextField(hint: 'Email', controller:emailController,isObscure: false, icon: Icons.email_outlined,),
+                              const SizedBox(height: 20,),
+                              AuthTextField(hint: 'Password', controller:passwordController,isObscure: true, icon: Icons.lock_outline,),
+                              const SizedBox(height: 40,),
+                              ElevatedButton(
+                                onPressed: (){
+                                  String emptyField = validate();
+                                  if(emptyField=='null'){
+                                    context.read<AuthBloc>().add(
+                                        AuthSignIn(
+                                            email: emailController.text.trim(),
+                                            password: passwordController.text.trim()
+                                        )
+                                    );
+                                  } else {
+                                    showSnackbar(context, '$emptyField is empty!');
+                                  }
+                                },
+                                style: ElevatedButton.styleFrom(
+                                  fixedSize: Size(MediaQuery.of(context).size.width,60),
+                                  backgroundColor: Colors.black,
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                                ),
+                                child: const Text(
+                                  'Sign In',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: ColorPalette.primary,
+                                      fontWeight: FontWeight.w500
+                                  ),
+                                )
+                              ),
+                              const SizedBox(height: 20,),
+                              GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => const SignUpPage()));
+                                },
+                                child: RichText(
+                                  text: TextSpan(
+                                    text: 'Don\'t have an account? ',
+                                    style:Theme.of(context).textTheme.titleMedium?.copyWith(
+                                        color: Colors.white
+                                    ),
+                                    children: const [
+                                      TextSpan(
+                                          text: 'Sign Up',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              color: ColorPalette.primary
+                                          )
+                                      )
+                                    ]
+                                  ),
                                 ),
                               )
-                            ),
-                            const SizedBox(height: 20,),
-                            GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => const SignUpPage()));
-                              },
-                              child: RichText(
-                                text: TextSpan(
-                                  text: 'Don\'t have an account? ',
-                                  style:Theme.of(context).textTheme.titleMedium?.copyWith(
-                                      color: Colors.white
-                                  ),
-                                  children: const [
-                                    TextSpan(
-                                        text: 'Sign Up',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold,
-                                            color: ColorPalette.primary
-                                        )
-                                    )
-                                  ]
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                    ),
+                            ],
+                          ),
+                                            ),
+                      ),
                   ),
                 ),
               ),
