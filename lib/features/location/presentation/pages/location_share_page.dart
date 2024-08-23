db90ddbffdc55f62e9b10a0876f8892061644549
amanhere-01 <amanhere0101@gmail.com>
2024-08-23 23:51:16 +0530

    UI change

--- lib/features/location/presentation/pages/location_share_page.dart
+++ lib/features/location/presentation/pages/location_share_page.dart
@@ -60,89 +60,93 @@ class _LocationSharePageState extends State<LocationSharePage> {
           }
           if (state is LocationLoaded) {
             Position currentPosition = state.position;
-            return Scaffold(
-              appBar: AppBar(
-                title: const Text('Sharing location'),
-                backgroundColor: ColorPalette.lightGreen1 ,
-              ),
-              body: Column(
-                children: [
-                  Expanded(
-                    child: Stack(
-                      children:[
-                        ShareLocationPageMap(
-                          longitude: currentPosition.longitude,
-                          latitude: currentPosition.latitude,
-                        ),
-                        Container(
-                          alignment: Alignment.topCenter,
-                          margin: const EdgeInsets.all(10),
-                          child: Card(
-                            elevation: 10,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15)
-                            ),
-                            color: ColorPalette.lightGreen1,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 5),
-                              child: RichText(
-                                text: TextSpan(
-                                    text: 'Your sharing code is: ',
-                                    style:  const TextStyle(
-                                      fontSize: 18,
-                                      color: Colors.black,
-                                    ),
-                                    children: [
-                                      TextSpan(
-                                          text: code,
-                                          style: TextStyle(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.bold,
-                                              color: ColorPalette.green2
-                                          )
-                                      )
-                                    ]
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ]
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 50,vertical: 5),
-                    child: SlideAction(
-                      height: 65,
-                      sliderRotate: false,
-                      innerColor: ColorPalette.primary,
-                      outerColor: ColorPalette.lightGreen1,
-                      elevation: 10,
-                      text: 'Start Sharing',
-                      textStyle: const TextStyle(
-                          fontSize: 20,
-                          color: Colors.black
-                      ),
-                      submittedIcon: const Icon(Icons.keyboard_double_arrow_right,size: 25),
-                      sliderButtonIcon: const Icon(Icons.keyboard_double_arrow_right, size: 25,),
-                      onSubmit: (){
-                        context.read<LocationBloc>().add(LocationShare(sharingCode: code, uid: widget.user.uid));
-                      },
-                    ),
-                  )
-                ],
-              ),
-            );
+            return _locationLoadedUI(currentPosition.longitude, currentPosition.latitude);
           }
           if (state is LocationSharing) {
-            return _buildLocationUI(state.longitude, state.latitude);
+            return _locationSharingUI(state.longitude, state.latitude);
           }
           return const Center(child: Text('Unexpected Error!'));
         },
       );
   }
 
-  Widget _buildLocationUI(double longitude, double latitude) {
+  Widget _locationLoadedUI(double longitude, double latitude){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sharing location'),
+        backgroundColor: ColorPalette.lightGreen1 ,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+                children:[
+                  ShareLocationPageMap(
+                    longitude: longitude,
+                    latitude: latitude,
+                  ),
+                  Container(
+                    alignment: Alignment.topCenter,
+                    margin: const EdgeInsets.all(10),
+                    child: Card(
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)
+                      ),
+                      color: ColorPalette.lightGreen1,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+                        child: RichText(
+                          text: TextSpan(
+                              text: 'Your sharing code is: ',
+                              style:  const TextStyle(
+                                fontSize: 18,
+                                color: Colors.black,
+                              ),
+                              children: [
+                                TextSpan(
+                                    text: code,
+                                    style: TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: ColorPalette.green2
+                                    )
+                                )
+                              ]
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ]
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50,vertical: 5),
+            child: SlideAction(
+              height: 65,
+              sliderRotate: false,
+              innerColor: ColorPalette.primary,
+              outerColor: ColorPalette.lightGreen1,
+              elevation: 10,
+              text: 'Start Sharing',
+              textStyle: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.black
+              ),
+              submittedIcon: const Icon(Icons.keyboard_double_arrow_right,size: 25),
+              sliderButtonIcon: const Icon(Icons.keyboard_double_arrow_right, size: 25,),
+              onSubmit: (){
+                context.read<LocationBloc>().add(LocationShare(sharingCode: code, uid: widget.user.uid));
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _locationSharingUI(double longitude, double latitude) {
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -203,13 +207,11 @@ class _LocationSharePageState extends State<LocationSharePage> {
                   color: ColorPalette.red2,
                   fontWeight: FontWeight.bold
                 ),
-                submittedIcon: Icon(Icons.close ,size: 25, color: ColorPalette.red1,),
+                submittedIcon: const Icon(Icons.close ,size: 25,),
                 sliderButtonIcon: Icon(Icons.close, size: 25, color: ColorPalette.red1,),
                 onSubmit: (){
                   context.read<LocationBloc>().add(LocationStopSharing());
                 },
-        
-        
               ),
             )
           ],
